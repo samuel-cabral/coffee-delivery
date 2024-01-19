b0182e74ba152c09f95178214605a45acec666a7
Samuel Cabral <samuelcabral.mail@gmail.com>
2024-01-19 02:23:19 -0300

    feat: apply global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/theme/default'
+
 export function App() {
   return (
-    <div>
+    <ThemeProvider theme={defaultTheme}>
       <h1>Hello World</h1>
-    </div>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
