d736fdf12c68c35a89f8a3399d26fdc21cfbb184
Samuel Cabral <samuelcabral.mail@gmail.com>
2024-02-03 20:08:10 -0300

    feat: apply notification badge in cart button

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { mixins } from '../../styles/mixins'
 
 export const HeaderContainer = styled.header`
   display: flex;
@@ -50,5 +51,28 @@ export const Aside = styled.aside`
 
     padding: 0.5rem;
     border-radius: 6px;
+
+    text-decoration: none;
+
+    position: relative;
+
+    span {
+      display: inline-flex;
+      height: 0.75rem;
+      width: 0.75rem;
+      padding: 0.6rem;
+
+      align-items: center;
+      justify-content: center;
+
+      border-radius: 50%;
+      background-color: ${({ theme }) => theme.colors['yellow-dark']};
+      color: ${({ theme }) => theme.colors.white};
+      ${mixins.fonts.textXS}
+
+      position: absolute;
+      top: -0.5rem;
+      right: -0.5rem;
+    }
   }
 `
