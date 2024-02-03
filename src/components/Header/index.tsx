66a3df47315d18d5401b574dfa7d4c4c6c920145
Samuel Cabral <samuelcabral.mail@gmail.com>
2024-02-03 19:47:18 -0300

    feat: add cart counter

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,8 +3,11 @@ import { Aside, HeaderContainer } from './styles'
 
 import { MapPin, ShoppingCart } from '@phosphor-icons/react'
 import logoImg from '../../assets/logo.svg'
+import { useCart } from '../../hooks/useCart'
 
 export function Header() {
+  const { cart } = useCart()
+
   return (
     <HeaderContainer>
       <div className="container">
@@ -18,8 +21,9 @@ export function Header() {
             <span>Porto Alegre, RS</span>
           </div>
 
-          <Link to="/cart" title="Carrinho">
+          <Link to={`cart`} aria-disabled={cart.length === 0}>
             <ShoppingCart size={22} weight="fill" />
+            {cart.length > 0 ? <span>{cart.length}</span> : null}
           </Link>
         </Aside>
       </div>
